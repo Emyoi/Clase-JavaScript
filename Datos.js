3e8949cd9c53c3b0fda14acd533bfe02fd122eda
CrisEmyoi <157163248+CrisEmyoi@users.noreply.github.com>
2024-08-21 11:20:43 -0600

    first commit

---
+++ Datos.js
@@ -0,0 +1,34 @@
+/* eslint-disable quotes */
+/* eslint-disable prettier/prettier */
+console.log("Hello World");
+
+// Variable String
+const nombre = "Juan";
+
+// Variable Number
+const numero = 5121;
+
+// Variable Boolean
+const isAdult = true;
+
+// Variable Null
+const something = null;
+
+console.log("----------------------------");
+console.log(nombre);
+console.log(numero);
+console.log(isAdult);
+console.log(something);
+
+console.log("----------------------------");
+
+// Objeto
+const person = {
+  name: `Carlos`,
+  lastname: `Rodriguez`,
+  age: 24,
+};
+
+console.log(person.name, person.lastname)
+
+// Undefined
