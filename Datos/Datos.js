78c271dd76a3b5c85e7826f3b61d57273d995482
CrisEmyoi <157163248+CrisEmyoi@users.noreply.github.com>
2024-08-21 11:58:34 -0600

    Se creo la carpeta de datos y iniciamos con los operadores

--- Datos/Datos.js
+++ Datos/Datos.js
@@ -25,10 +25,31 @@ console.log("----------------------------");
 // Objeto
 const person = {
   name: `Carlos`,
-  lastname: `Rodriguez`,
+  lastName: `Rodriguez`,
   age: 24,
 };
 
-console.log(person.name, person.lastname)
+console.log(person.name, person.lastName);
 
 // Undefined
+console.log(person.middleName);
+
+// Array
+const numbers = [5, 6, 78, 43, 4];
+
+console.log(numbers);
+
+// Function
+
+function saludar() {
+  return "Hola";
+}
+
+console.log(saludar());
+
+// Date
+const hoy = new Date();
+
+console.log(hoy)
+
+// 
---
+++ Operadores/operators.js
@@ -0,0 +1,29 @@
+//! Operadores Aritmeticos
+
+// Creacion de una funcion que sume 2 valores
+function suma(a, b) {
+  return a + b;
+}
+
+// Creacion de una funcion que reste 2 valores
+function resta(a, b) {
+  return a - b;
+}
+
+// Creacion de una funcion que divida 2 valores
+function division(a, b) {
+  return a / b;
+}
+
+// Creacion de una funcion que multiplique 2 valores
+function multiplicacion(a, b) {
+  return a * b;
+}
+
+// Creacion de una funcion que de el residuo de 2 valores
+function residuo(x, y) {
+  const z = x % y;
+  return z;
+}
+
+//! Operadores de Asignacion
