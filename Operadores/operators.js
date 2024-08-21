b79ef43c3ce63999d5fb467033ea12bc4ddcd72f
CrisEmyoi <157163248+CrisEmyoi@users.noreply.github.com>
2024-08-21 12:59:32 -0600

    se Agregaron nuevos tipos de operadores"

--- Operadores/operators.js
+++ Operadores/operators.js
@@ -27,3 +27,32 @@ function residuo(x, y) {
 }
 
 //! Operadores de Asignacion
+let x = 5;
+
+// =
+const isMarried = true;
+
+// +=
+x += 5;
+console.log(x);
+
+// -=
+x -= 6;
+console.log(x);
+
+// *=
+x *= 2;
+console.log(x);
+
+// /=
+x /= 2;
+console.log(x);
+
+// %=
+x %= 2;
+console.log(x);
+
+x = 4;
+// **=
+x **= 2;
+console.log(x);
