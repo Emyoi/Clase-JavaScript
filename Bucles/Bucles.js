eac71a860e854b70f540ee4278f96ef6edfa2f71
CrisEmyoi <157163248+CrisEmyoi@users.noreply.github.com>
2024-08-22 18:51:07 -0600

    El DOOM

---
+++ Bucles/Bucles.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-plusplus */
+//! Bucle For
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+console.log(`****************************************`);
+
+// for (let x = 5; x >= 0; x--) {
+//     console.log(x);
+// }
+
+console.log(`****************************************`);
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`El resultado de ${i} * ${i} es ${i * i}`);
+//
+
+//! Bucle For Of
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// for (const num of numbers) {
+//   console.log(num * num);
+// }
+
+//! Bucle While
+// let i = 0;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+//! Bucle do While
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+// ?    Break
+// for (let i = 0; i <= 5; i++) {
+//   if (i === 3) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// ?    Continue
+// for (let i = 0; i <= 5; i++) {
+//   if (i === 3) {
+//     continue;
+//   }
+//   console.log(i);
+// }
